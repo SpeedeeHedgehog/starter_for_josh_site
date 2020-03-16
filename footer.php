c598eb448b7b5bed2730d3dd2530260e89d01027
SpeedeeHedgehog <61498262+SpeedeeHedgehog@users.noreply.github.com>
2020-03-16 21:48:34 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer class="container-fluid"> <!--for main image to go to edge of page-->
+          <div class="container"> <!--div for holding header content in centre-->
+              <div class="row"> <!--row to get access to bootstrap columns-->
+                  <div class="col-md-6 text-center footer-content">
+                      <p><?php bloginfo('name'); ?></p>
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt="tumblr">
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt="rss">
+                  </div>
+                  
+                  <div class="col-md-6 text-center footer-content">
+                      <p>Terms and Conditions</p>
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg" alt="linkedin">
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/facebook.jpg" alt="facebook">
+                  </div>
+              
+              
+              </div>
+          
+          
+          </div>
+      
+      
+      </footer>
+  </body>
+</html>
+<?php wp_footer(); ?>
