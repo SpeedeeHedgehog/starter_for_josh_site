8c31d5ec4cd8ae8f6e62eb2bada5ca3a8dc92852
SpeedeeHedgehog <61498262+SpeedeeHedgehog@users.noreply.github.com>
2020-03-16 22:20:00 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
                   <div class="col-md-6 text-center footer-content">
                       <p><?php bloginfo('name'); ?></p>
                       <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt="tumblr">
-                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt="rss">
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt="rss">
                   </div>
                   
                   <div class="col-md-6 text-center footer-content">
